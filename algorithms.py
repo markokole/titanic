095d9edb48a05bef7976c6c1f2272cc4c66d81be
markokole <marko_kole@yahoo.com>
2017-08-09 15:42:20 +0200

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -37,12 +37,19 @@ def gauss(X, y, Xpred):
 
 ##Decision Tree Classifier
 #Regularization - restrict the Decision Tree's freedom during training - by using hyperparameters
-def tree(X, y, Xpred, type, file_name, sample_split):
+def tree(X, y, Xpred, type, file_name):
     from sklearn.tree import DecisionTreeClassifier
     from utility import generate_graphics
-    #tree_clf = DecisionTreeClassifier(min_samples_split=sample_split)
-    #tree_clf = DecisionTreeClassifier(min_samples_leaf=10, min_samples_split=30, max_depth=4, criterion="gini")
-    tree_clf = DecisionTreeClassifier(max_depth=5, criterion="gini", random_state=15, presort=True)
+
+    tree_clf = DecisionTreeClassifier(
+                                      max_depth=5
+                                     ,criterion="gini" #entropy performs slighty worse
+                                     ,random_state=15
+                                     ,presort=True
+                                     ,class_weight={0:.5, 1:.5} #default
+                                     )
+
+    #print tree_clf.class_weight
     '''
     #is "samples" on the graphic tree
     print "min_samples_split", tree_clf.min_samples_split
@@ -64,14 +71,6 @@ def tree(X, y, Xpred, type, file_name, sample_split):
     feature_names = list(X.columns)
     target_names = ["0","1"]
     generate_graphics(type, file_name, tree_clf, feature_names, target_names)
-
-    return pred
-
-def random_forest(X, y, Xpred):
-    from sklearn.ensemble import RandomForestClassifier
-    clf = RandomForestClassifier(n_estimators=500, n_jobs=-1)
-    clf.fit(X, y.values.ravel())
-    pred = clf.predict(Xpred)
 
     return pred
 
